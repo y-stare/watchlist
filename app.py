d975ec648bf4d61c79485f5164c4a3e039fad8e9
cty <1210917207@qq.com>
2025-08-07 20:47:23 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,36 +1,82 @@
+import click
+import os
+import sys
 from flask import Flask,url_for,render_template
+from flask_sqlalchemy import SQLAlchemy
 from markupsafe import escape
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix="sqlite:///"
+else:
+    prefix="sqlite:////"
+
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+db = SQLAlchemy(app)
+@app.cli.command() # 注册为命令，可以传入 name 参数来自定义命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
 name = 'Grey Li'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):  # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+
+    year = db.Column(db.String(4))  # 电影年份
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+    name='Grey Li'
+    movies = [
+       {'title': 'My Neighbor Totoro', 'year': '1988'},
+       {'title': 'Dead Poets Society', 'year': '1989'},
+       {'title': 'A Perfect World', 'year': '1993'},
+       {'title': 'Leon', 'year': '1994'},
+       {'title': 'Mahjong', 'year': '1996'},
+       {'title': 'Swallowtail Butterfly', 'year': '1996'},
+       {'title': 'King of Comedy', 'year': '1999'},
+       {'title': 'Devils on the Doorstep', 'year': '1999'},
+       {'title': 'WALL-E', 'year': '2008'},
+       {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
 @app.route('/')
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    user=User.query.first()
+    movies=Movie.query.all()
+    return render_template('index.html', user=user, movies=movies)
 
-@app.route('/user/<name>')
-def user_page(name):
-    return f'User: {escape(name)}'
-@app.route('/test')
-def test_url_for():
+#@app.route('/user/<name>')
+#def user_page(name):
+    #return f'User: {escape(name)}'
+#@app.route('/test')
+#def test_url_for():
     # 下面是一些调用示例（请访问 http://localhost:5000/test 后在命令行窗口查看输出的 URL）：
-    print(url_for('hello'))  # 生成 hello 视图函数对应的 URL，将会输出：/
+    #print(url_for('hello'))  # 生成 hello 视图函数对应的 URL，将会输出：/
     # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
-    print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
-    print(url_for('user_page', name='peter'))  # 输出：/user/peter
-    print(url_for('test_url_for'))  # 输出：/test
+    #print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
+    #print(url_for('user_page', name='peter'))  # 输出：/user/peter
+    #print(url_for('test_url_for'))  # 输出：/test
     # 下面这个调用
-    print(url_for('test_url_for', num=2))  
-    return 'Test page'
-if __name__ == '__main__':
-    app.run(debug=True)
+    #print(url_for('test_url_for', num=2))  
+    #return 'Test page'
+#if __name__ == '__main__':
+    #app.run(debug=True)
